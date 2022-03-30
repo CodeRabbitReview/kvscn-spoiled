633672657b4704aa247ac71c031f41bf4a88ea0a
mykhailo.prokopchyk <mykhailo.prokopchyk@yalantis.net>
2022-03-30 14:13:01 +0300

    add storage type that is a key-value database add Pair type Add put, get, delete methods and description to they

---
+++ internal/storage/storage.go
@@ -0,0 +1,43 @@
+package storage
+
+import "errors"
+
+var NoSuchValue = errors.New("no such value by this key")
+
+type Key string
+
+type Pair struct {
+	Key   Key
+	Value interface{}
+}
+
+type Storage struct {
+	pairs map[Key]interface{}
+}
+
+func NewStorage() *Storage {
+	return &Storage{
+		pairs: make(map[Key]interface{}),
+	}
+}
+
+//Put add new value to storage
+func (s *Storage) Put(p *Pair) {
+	s.pairs[p.Key] = p.Value
+}
+
+// Get returns a copy of value from storage
+// If no such data by key returns NoSuchValue error
+func (s *Storage) Get(k Key) (interface{}, error) {
+	if v, ok := s.pairs[k]; ok {
+		return v, nil
+	}
+
+	return nil, NoSuchValue
+}
+
+// Delete remove data from storage
+// A key is ignored if it does not exist
+func (s *Storage) Delete(k Key) {
+	delete(s.pairs, k)
+}
