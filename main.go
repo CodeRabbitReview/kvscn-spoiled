db256555415875461d6b9ae405fbe798e9044348
mykhailo.prokopchyk <mykhailo.prokopchyk@yalantis.net>
2022-04-01 14:07:32 +0300

    add main func to test by hands

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mishaprokop4ik/storage/internal/storage"
+	"log"
+)
 
+var s = storage.NewStorage()
+
+func main() {
+	err := s.Put(storage.Pair{
+		Key:   "simple",
+		Value: "hello there",
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	v, err := s.Get("simple")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(v)
 }
