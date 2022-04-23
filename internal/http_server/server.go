f77da0e18234cd0e022cf89ea662520d7a641800
mykhailo.prokopchyk <mykhailo.prokopchyk@yalantis.net>
2022-04-23 13:26:00 +0300

    add desc to http server run method

--- internal/http_server/server.go
+++ internal/http_server/server.go
@@ -37,7 +37,9 @@ type resumer interface {
 	SendRecovered(addr string)
 }
 
-//Run ..
+// Run runs http server and take recovered data and send it
+// to server in parallel.
+// Run catch system signal and display it
 func (s *HTTPServer) Run(r resumer) {
 	go func() {
 		if err := s.server.ListenAndServe(); err != nil {
