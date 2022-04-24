6b40f4e568ab504fb1349eb5becb0ffd8a629fd4
mykhailo.prokopchyk <mykhailo.prokopchyk@yalantis.net>
2022-04-24 14:37:21 +0300

    change http to https in client url

--- internal/recoverer/transaction_logger.go
+++ internal/recoverer/transaction_logger.go
@@ -127,7 +127,7 @@ func (r *TransactionLogger) takeRecovered() {
 // first requests will put after it delete
 func (r *TransactionLogger) SendRecovered(port string) {
 	go r.takeRecovered()
-	client := client.NewAPI(fmt.Sprintf("http://localhost%s", port))
+	client := client.NewAPI(fmt.Sprintf("https://localhost%s", port))
 	defer close(r.c)
 	wg := &sync.WaitGroup{}
 	var toDelete []string
