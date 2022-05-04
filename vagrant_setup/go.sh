f0b730fa5d77dbc123e023a5759cd4902116a1af
mishaprokop4ik <73275046+mishaprokop4ik@users.noreply.github.com>
2022-05-04 17:02:51 +0300

    updated go init in Vagrant

--- vagrant_setup/go.sh
+++ vagrant_setup/go.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-export GOPATH=$HOME/work
 sudo curl -O https://storage.googleapis.com/golang/go1.18.1.linux-amd64.tar.gz
 sudo tar -xvf go1.18.1.linux-amd64.tar.gz
 sudo mv go /usr/local
+sudo echo 'export GOROOT=/usr/local/go' >> /etc/profile
 sudo echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
-export PATH=$PATH:/usr/local/go/bin
+sudo echo 'export GOPATH=/usr/local' >> /etc/profile
+sudo echo 'export PATH=$PATH:$GOPATH/bin' >> /etc/profile
+sudo echo 'export GO111MODULE=on' >> /etc/profile
+source /etc/profile
 sudo rm go1.18.1.linux-amd64.tar.gz
+go install honnef.co/go/tools/cmd/staticcheck@latest
+sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.45.0
+cd /workspace/storage && go mod download
